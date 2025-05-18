68a960fb2e9fd27435e09c96adba71e51f8d9ee8
manuvaladares <manuella.valadares@hotmail.com>
2025-05-18 11:21:56 -0300

    refactor About component: update image assets and enhance layout with new photo placements and hover effects

--- src/components/About/styled.js
+++ src/components/About/styled.js
@@ -26,11 +26,11 @@ export const AboutText = styled.div`
   }
 `;
 
-export const PhotoLeft = styled.img`
+export const PhotoLeftup = styled.img`
   position: absolute;
   top: -40px;
   left: -400px;
-  width: 400px;
+  width: 300px;
   transform: rotate(-12deg);
   border: 1px solid #ddd;
   box-shadow: 0 8px 20px rgba(0, 0, 0, 0.25);
@@ -43,11 +43,42 @@ export const PhotoLeft = styled.img`
 
 `;
 
-export const PhotoRight = styled.img`
+export const PhotoRightdown = styled.img`
   position: absolute;
   bottom: -40px;
   right: -400px;
+  width: 300px;
+  transform: rotate(-12deg);
+  border: 1px solid #ddd;
+  box-shadow: 0 8px 20px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+  &:hover {
+    transform: rotate(0deg) scale(1.1);
+    transition: transform 0.4s ease;
+`;
+
+export const PhotoLeftdown = styled.img`
+  position: absolute;
+  bottom: -10px;
+  left: -400px;
   width: 400px;
+  transform: rotate(10deg);
+  border: 1px solid #ddd;
+  box-shadow: 0 8px 20px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+
+  &:hover {
+    transform: rotate(0deg) scale(1.1);
+    transition: transform 0.4s ease;
+}
+
+`;
+
+export const PhotoRightup = styled.img`
+  position: absolute;
+  top: -40px;
+  right: -400px;
+  width: 300px;
   transform: rotate(10deg);
   border: 1px solid #ddd;
   box-shadow: 0 8px 20px rgba(0, 0, 0, 0.25);
