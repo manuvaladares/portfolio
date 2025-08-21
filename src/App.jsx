9054126ffdd1c3c5da7f7e2a5901d74596d2d881
manuvaladares <manuella.valadares@hotmail.com>
2025-08-21 10:52:21 -0300

    defini as cores e a fonte direto no root do global.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-
-// Importando as páginas
 import AboutPage from './pages/AboutPage/AboutPage';
-
-// Estilos globais
 import './assets/styles/global.css';
 
 function App() {
   return (
     <Routes>
-      {/* Rota padrão redireciona para "/about" */}
       <Route path="/" element={<AboutPage />} />
       <Route path="/about" element={<AboutPage />} />
       {/* <Route path="/work" element={<WorkPage />} />
